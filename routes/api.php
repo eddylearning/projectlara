8dcd64c2009fd4ec2b40a91e48a3069df8f58909
eddylearning <157018938+eddylearning@users.noreply.github.com>
2025-10-31 13:15:22 -0700

    ptoteted admin using middlware

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//protected route for admins
+Route::middleware(['auth','admin'])->group(function(){
 Route::apiResource('car', CarApiController::class);
+});
