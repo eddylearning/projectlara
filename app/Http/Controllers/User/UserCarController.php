53f63d96440dc6f6501d2a12a056b77229c3d176
eddylearning <157018938+eddylearning@users.noreply.github.com>
2025-11-11 12:39:39 -0800

    added controller for user cars to show booked cars

---
+++ app/Http/Controllers/User/UserCarController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserCarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    // List all available cars
+    public function index()
+    {
+        $cars = Car::where('available', true)->get();
+        return view('user.cars.index', compact('cars'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    // Show details for a single car
+    public function show(Car $car)
+    {
+        return view('user.cars.show', compact('car'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
